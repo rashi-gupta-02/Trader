628acf78310ba8326989f9d304e091db36694847
rashi-gupta-02 <rashi@equantra.in>
2025-05-22 11:50:24 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,1 @@
-from fastapi import FastAPI
-from app.db.session import init_db
 
-app = FastAPI()
-
-@app.on_event("startup")
-async def on_startup():
-    await init_db()
-
-@app.get("/")
-async def root():
-    return {"status": "API is running!"}
